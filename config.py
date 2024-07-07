4cfc638c14a3e696c2af628d6f12073ac51cd50e
Gunnugupta <166784443+Gunnugupta@users.noreply.github.com>
2024-07-07 16:36:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "21551881")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "6e83e9e1aee5accd4868dc29aa59ebaa") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7438164515:AAGZankxbqJLVOfn7fyXHfP_0jQtGI-fDmA") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '-1002221767070') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://david:surya@cluster0.s7o0tyw.mongodb.net/")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
 
     # Other Configs 
